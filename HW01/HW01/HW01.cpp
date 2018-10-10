4789c973343943fb3372a179ad0a3a9feb2a34a5
yunnhec <36875922+yunnhec@users.noreply.github.com>
2018-10-10 19:51:37 +0800

    20181010 19:51

--- HW01/HW01/HW01.cpp
+++ HW01/HW01/HW01.cpp
@@ -3,19 +3,51 @@
 
 #include "stdafx.h"
 #include <iostream>
-
+#include <time.h>
 using namespace std;
 
-template<class Max> Max find_max(Max *data, int size){
-	Max result = data[0];
+template<class T> T find_max(T *data, int size){
+	T result = data[0];
 	for (int i = 1; i < size; i++){
 		if (result < data[i])
 			result = data[i];
-		return result;
 	}
+		return result;
+}
 
-	int _tmain(int argc, _TCHAR* argv[])
+	int main()
 	{
+		srand(time(NULL));
+		short *test = new short;
+		for (int i = 0; i < 6; i++){
+			test[i] = rand()%1000;
+		}
+		cout << "Array of 6 short values: " << endl;
+		for (int i = 0; i < 6; i++){
+			cout << test[i] <<'\t';
+			if (i == 5)
+				cout << endl;
+		}
+		cout << "Maximun: " << find_max(test, 6) << endl;
+		cout << "----------------------------------------------" << endl;
+
+		double upB = 100.0, lowB = 10.0; //defind upper bound and lower bound (0~10)
+		double *test2 = new double;
+		for (int i = 0; i < 6; i++){
+			test2[i] = rand() / (RAND_MAX+1);
+			//test2[i] = lowB + test2[i] * (upB - lowB);
+		}
+		/*cout << "Array of 6 short values: " << endl;
+		for (int i = 0; i < 6; i++){
+			cout << test2[i] << '\t';
+			if (i == 5)
+				cout << endl;
+		}*/
+		//cout << "Maximun: " << find_max(test2, 6) << endl;
+
+		
+
+		system("pause");
 		return 0;
 	}
 
